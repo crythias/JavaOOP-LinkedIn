4d5709ebe461500cfb84a3b0c1d51bdc27f9fd63
Gerald Young <help@gwy.org>
2025-08-12 14:39:33 -0400

    Created instances, including announceTallTree method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Tree myFavoriteOaktree = new Tree(25,5, TreeType.OAK);
-        System.out.println("My favorite tree is a " + myFavoriteOaktree.treeType + 
-                           " with a height of " + myFavoriteOaktree.heightFt + 
-                           " feet and a trunk diameter of " + myFavoriteOaktree.trunkDiameterIn + 
+        Tree myFavoriteOakTree = new Tree(120,12, TreeType.OAK);
+        System.out.println("My favorite tree is a " + myFavoriteOakTree.treeType + 
+                           " with a height of " + myFavoriteOakTree.heightFt + 
+                           " feet and a trunk diameter of " + myFavoriteOakTree.trunkDiameterIn + 
                            " inches.");
+
+        Tree myFavoriteMapleTree    = new Tree(90, 30, TreeType.MAPLE);    
+        myFavoriteMapleTree.announceTallTree();
+        myFavoriteOakTree.announceTallTree();
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -7,11 +7,21 @@ public class Tree {
         this.heightFt = heightFt;   // height in feet
         this.trunkDiameterIn = trunkDiameterIn; // trunk diameter in inches
         this.treeType = treeType; // type of tree
-        
+      
     }
+    
     void grow() {
+        // Method to simulate tree growth
         this.heightFt += 10.0; // height grows by 10 feet
         this.trunkDiameterIn = this.trunkDiameterIn + 1.0; // trunk diameter grows by 1.0 inches
 
+    }
+
+    void announceTallTree() {
+        if (this.heightFt > 100) {
+            System.out.println("Wow, that's a tall " + this.treeType + " tree!");
+        } else {
+            System.out.println("That's a nice " + this.treeType + " tree, but not too tall.");
+        }
     }
 }
